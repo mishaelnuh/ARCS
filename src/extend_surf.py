57385f29ffecb2a4082dd1332dac14a33b1c3747
Mishael Nuh <mishael.ebel.nuh@gmail.com>
2021-01-13 14:14:10 +0000

    Extract extend_surf module

---
+++ src/extend_surf.py
@@ -0,0 +1,29 @@
+import Rhino.Geometry as rg
+import Rhino.RhinoDoc as rd
+import math
+from System import Array
+
+def extend_surf(surf, plane):
+    tol = rd.ActiveDoc.ModelAbsoluteTolerance
+
+    # Get rotated plane to 45 deg
+    rotated_plane = plane.Clone()
+    rotated_plane.Rotate(math.pi / 4, rg.Vector3d(0, 0, 1))
+
+    # Calculate large boundary to extend to using consecutive bounding boxes
+    bounds = rg.Box(plane, surf.GetBoundingBox(plane))
+    bounds = rg.Box(rotated_plane, bounds.GetCorners())
+    bounds = rg.Box(plane, bounds.GetCorners())
+    bounds_corners = bounds.GetCorners()
+
+    # Find a boundary polyline curve to extend to
+    extended_outline = rg.Polyline([bounds_corners[i] for i in (0, 4, 5, 1, 0)])
+    extended_outline = extended_outline.ToPolylineCurve()
+
+    # Extend surface using Brep patch
+    extended_surf = rg.Brep.CreatePatch(Array[rg.GeometryBase]([extended_outline,
+        rg.Mesh.CreateFromBrep(surf)[0]]), None, 20, 20, False, True,
+        extended_outline.GetLength() / 100, 50, 10, 
+        Array[bool]([True, True, True, True]), tol)
+
+    return extended_surf
